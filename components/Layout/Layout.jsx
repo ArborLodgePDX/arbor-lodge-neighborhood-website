b8ccc02bd1e2e7db45a79c4d75246c340071edbc
Dusty McCord <dusty@Dustys-MacBook-Pro.local>
2023-02-11 12:47:27 -0800

    move gcal above map swap out iframe link

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -33,20 +33,20 @@ function Layout({ children, menu, page, signup, neighborhoods, show, slug }) {
           <EmailSignup content={signup} />
 
           <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&mode=AGENDA&showPrint=0&showTz=1&showCalendars=0&showTabs=0&showTitle=0&src=cHVibGljQGFyYm9ybG9kZ2VuZWlnaGJvcmhvb2QuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
+            style={{ "border": "solid 1px #777" }}
+            width="100%"
+            height="350"
+            frameborder="0"
+          />
+          <br /><br />
+          <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d22343.47670235109!2d-122.693268!3d45.571754!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a7a3b74d15c1%3A0xf60b1b34aa1f920b!2sArbor%20Lodge%2C%20Portland%2C%20OR%2097217!5e0!3m2!1sen!2sus!4v1645495845525!5m2!1sen!2sus"
             width="100%"
             height="350"
             style={{ border: 0 }}
             loading="lazy"
           />
-          <iframe
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&mode=MONTH&showNav=1&showTitle=0&showCalendars=0&src=cHVibGljQGFyYm9ybG9kZ2VuZWlnaGJvcmhvb2QuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73&color=%230B8043"
-            style={{ "border": "solid 1px #777" }}
-            width="100%"
-            height="350"
-            frameborder="0"
-            scrolling="no"
-          ></iframe>
           <br /><br />
           <PageText title={neighborhoods.title} content={neighborhoods.text} />
         </section>
