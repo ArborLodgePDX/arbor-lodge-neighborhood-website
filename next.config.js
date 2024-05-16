2711abf134d890c5268ead20bf0a5a0b03b359b9
Kevin <kevin@micalizzi.com>
2024-05-16 10:06:53 -0700

    adding /survey redirect

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ module.exports = {
         destination: 'https://www.paypal.com/donate?hosted_button_id=UY3SHVCSZZRZA',
         permanent: true,
       },
+      {
+        source: '/survey',
+        destination: 'https://docs.google.com/forms/d/e/1FAIpQLScccKWR3GQDFQwvDLvRGmFEoj-wDqHfLP0q2ReF3lIs4a0PQQ/viewform',
+        permanent: true,
+      },
     ];
   },
 };
